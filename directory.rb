348c6202e6c8ad7ae8f949e054254ced28501bee
Harry Parsons <hparsons93@gmail.com>
2021-08-17 10:55:55 +0100

    Villanious methods

--- directory.rb
+++ directory.rb
@@ -11,20 +11,21 @@ students = [
   "The Joker", "Joffrey Baratheon", 
   "Norman Bates"] 
 
-puts "The Students of Villains Academy"
-puts "-----------" 
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
 
-puts "Overall, we have #{student_count} great students"
+def print_header
+  puts "The Students of Villains Academy"
+  puts "-----------" 
+end 
+
+def print(names)
+names.each {|name| puts name}
+end 
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end 
 
 
+print_header 
+print(students)
+print_footer(students)
