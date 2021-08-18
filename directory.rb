5900cf3495436a794b7afa6a2b76162733613ce2
Harry Parsons <hparsons93@gmail.com>
2021-08-18 15:41:36 +0100

    boom

--- directory.rb
+++ directory.rb
@@ -78,8 +78,8 @@ def save_students
 
 end 
 
-def load_students 
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line| 
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
